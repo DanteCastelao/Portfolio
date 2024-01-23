ee2cab6e2d25001e61acc51792b39fd780bd068e
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2024-01-23 12:27:27 -0300

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import partytown from '@astrojs/partytown'
 
 export default defineConfig({
-    integrations: [react()]
+    integrations: [react(),partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),]
   });
