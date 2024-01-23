b8d1cef08d9467da851872df4aa4b37b74c1f1e9
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2024-01-23 17:02:42 -0300

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [react(),partytown({
-			config: {
-			  forward: ["dataLayer.push"],
-			},
-		}),]
-  });
+	site: 'https://dantecastelao.com.ar',
+	integrations: [react(), partytown({
+		config: {
+		forward: ["dataLayer.push"]
+		}
+	}), sitemap()]
+});
