a6ffaaaa42d8d914ee7a6dd53d5244702801e171
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2024-04-04 10:45:48 -0300

    Start Menu

---
+++ portfolio/components/Menu/Menu.tsx
@@ -0,0 +1,58 @@
+import './Menu.css';
+import shutdownIcon from '../../assets/shutdownicon.png';
+
+export default function Menu() {
+    return (
+        <div className="menu">
+            <div className='wordmark'/>
+            <div className='menu-list'>
+                <div className='programs'>
+                    <div className='program'>
+                        <img src={shutdownIcon} alt="Shutdown icon"/>
+                        <span>Projects</span>
+                        <div className="submenu">
+                            <div className='sublist'>
+                                <div>
+                                    <img src={shutdownIcon} alt="Shutdown icon"/>
+                                    <span>Zone</span>
+                                </div>
+                                <div>
+                                    <img src={shutdownIcon} alt="Shutdown icon"/>
+                                    <span>Nomore</span>
+                                </div>
+                                <div>
+                                    <img src={shutdownIcon} alt="Shutdown icon"/>
+                                    <span>Agorar</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='program'>
+                        <img src={shutdownIcon} alt="Shutdown icon"/>
+                        <span>Programs</span>
+                        <div className="submenu">
+                            <div className='sublist'>
+                                <div>
+                                    <img src={shutdownIcon} alt="Shutdown icon"/>
+                                    <span>Music</span>
+                                </div>
+                                <div>
+                                    <img src={shutdownIcon} alt="Shutdown icon"/>
+                                    <span>Livestream</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='program'>
+                        <img src={shutdownIcon} alt="Shutdown icon"/>
+                        <span>About me</span>
+                    </div>
+                </div>
+                <div className='shutdown-button'>
+                    <img src={shutdownIcon} alt="Shutdown icon"/>
+                    <span>Shut Down...</span>
+                </div>
+            </div>
+        </div>
+    )
+}
--- portfolio/components/TaskBar/TaskBar.tsx
+++ portfolio/components/TaskBar/TaskBar.tsx
@@ -1,20 +1,48 @@
 import './TaskBar.css';
 import win95logo from '../../assets/win95logo.png';
+import { useEffect, useState } from 'react';
+import Menu from '../Menu/Menu';
 
 export default function TaskBar() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    useEffect(() => {
+        const handleOutsideClick = (event: { target: { closest: (arg0: string) => any; }; }) => {
+            if (menuOpen && !event.target.closest('.menu') && !event.target.closest('.start-button')) {
+                setMenuOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [menuOpen]);
+
     return (
-        <div className="taskbar">
-            <div className="start-button">
-                <div className='subcontainer'>  
-                    <img className='winlogo' src={win95logo} alt="Windows 95 logo" />
-                    <span>Start</span>
+        <>
+            {menuOpen && <Menu />}
+            <div className="taskbar">
+                <div className="start-button" onClick={toggleMenu} tabIndex={0}>
+                    <div className='subcontainer'>
+                        <div className='border'>
+                            <img className='winlogo' src={win95logo} alt="Windows 95 logo" />
+                            <span>Start</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="taskbar-icons">
+                </div>
+                <div className="taskbar-time">
+                    <span>10:00</span>
                 </div>
             </div>
-            <div className="taskbar-icons">
-            </div>
-            <div className="taskbar-time">
-                <span>10:00</span>
-            </div>
-        </div>
+        </>
     )
 }
