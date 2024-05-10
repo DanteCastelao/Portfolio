65b3781e90756582a2ec89746f9cccd30b2cebc3
Dante Castelao <34772417+DanteCastelao@users.noreply.github.com>
2024-05-10 16:58:46 -0300

    Update Livestream.tsx

--- portfolio/components/Livestream/Livestream.tsx
+++ portfolio/components/Livestream/Livestream.tsx
@@ -4,7 +4,7 @@ export default function Livestream() {
     return (
         <div className='livestream'>
             <video autoPlay loop muted>
-                <source src='//cdn-cf-east.streamable.com/video/mp4/o9uuse.mp4?Expires=1714511280&Signature=Kr92petm7vbp9Or82aLNhyKgOEnwuVqTaGI0J7wIuxRLzsuPa2xIXp0lvoIIwYmxPORa-YFzcjKdrwDSqb7Nr1wDFccIXFZ5g~HHnUgUnwoCJk6HHg4io8m5AExqMBQsEQUnjYo11MyxddsF3Ssnq3-zR7hXpz02PaSXGYbsjvHCZ9LF8TvUpz-q4hyBs8UDtP5VVc1NkY0CoVudn6BnFUede0V5WeKNkoZk6Sg5fhPuBb5wWOiqWOtUAIQBqJ6VsCL4Djy3jbEFd1slgKT27lEZUVf2rSZbd7qRNJQ7tF60iR5JtiRzXetJX3RCoar71~adfYOOlf4za3nX9ltAgQ__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ' type='video/mp4' />
+                <source src="//cdn-cf-east.streamable.com/video/mp4/o9uuse.mp4?Expires=1715629740&Signature=SHnHoj4P4I45lIKenp~-DCidteRZa7cOHVIaM2REsyDoLr~SsdBvk3NnTLoHNDUeAYg1aUxIFWH0oeAlNvrhISy26wQcDZNSX425una8rCe9pSy3k~JBA5Wqda0bPqwRs5GKZnAV6y8bBGNBWzBsY3AXvKf~Mpq5Nf0bHNU~PSmQn5ikZAgZ0inGF9PwisaHwYMzLUxSBzl8Fb7M1E4X7DFo5JsuZy7shNuY8M0dI5hyiEnT226Oxu8rzCtLs2FBXVgEUALRjWJMEynKyNinMyhRjuluDQCvUGpRprlUlawvXzHa9sXZ55vPQ-SkH6pXgpUjf27ld-eO6kqhxTs9Xg__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ" type='video/mp4' />
             </video>
         </div>    
     )
